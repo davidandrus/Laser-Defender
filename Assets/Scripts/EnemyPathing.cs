7475c250a9c7a3830ebafef8cf7f5bd0f624cf6d
David Andrus <davidandrus.com@gmail.com>
2020-05-21 08:55:39 -0700

    adding and attaching enemy pathing

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    // Start is called before the first frame update
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypointIndex <= waypoints.Count - 1) {
+            var moveDelta = moveSpeed * Time.deltaTime;
+            var targetPos = waypoints[waypointIndex].position;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, moveDelta);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
