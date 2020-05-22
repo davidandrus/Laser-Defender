4a459dc0cd3688933e2738628ca0e9100dc0bf24
David Andrus <davidandrus.com@gmail.com>
2020-05-21 22:10:23 -0700

    Add damage dealer and destroy dealer and enemy when health is too low

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float health = 100f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
